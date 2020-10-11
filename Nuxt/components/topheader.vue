19812cb07d87d36427a01b91a7ef26c59c47a8c5
unknown <aribahtiar@unsil.ac.id>
2020-10-11 12:08:25 +0700

    Install Bootwind Starter Template

---
+++ Nuxt/components/topheader.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="w-full px-3 fixed lg:px-20 pt-5 bg-theme_primary_light">
+           <!-- Header -->
+            <nav class="w-full flex flex-wrap" >
+                
+                    <nuxt-link to="/" class="mr-auto flex ">
+                        <div class="flex text-primary" >
+                            <span class="p-1 rounded-full bg-primary" >
+                                <img class="w-6 h-6 " src="/icon.png" alt="avatar" >
+                            </span>
+                            <span class="hidden lg:block px-2 font-bold text-xl"> 
+                                BootWind Starter
+                            </span> 
+                              <span class="block lg:hidden px-2 font-bold text-xl"> 
+                                 Bootwind
+                            </span> 
+                        </div>
+
+                    </nuxt-link>
+
+                    <div class="ml-auto flex">
+                          <div @click="$store.commit('toggleSetting')" class="
+                          border border-primary hover:bg-primary hover:text-secondary
+                          rounded-xl
+                          px-5 text-primary cursor-pointer  relative flex">
+                       
+                            <span class="p-1">Masuk</span>
+                        </div>
+                        <div @click="$store.commit('toggleSetting')" class="px-3 text-primary cursor-pointer relative flex">
+                            <svg class="bi bi-gear m-1" width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8.837 1.626c-.246-.835-1.428-.835-1.674 0l-.094.319A1.873 1.873 0 0 1 4.377 3.06l-.292-.16c-.764-.415-1.6.42-1.184 1.185l.159.292a1.873 1.873 0 0 1-1.115 2.692l-.319.094c-.835.246-.835 1.428 0 1.674l.319.094a1.873 1.873 0 0 1 1.115 2.693l-.16.291c-.415.764.42 1.6 1.185 1.184l.292-.159a1.873 1.873 0 0 1 2.692 1.116l.094.318c.246.835 1.428.835 1.674 0l.094-.319a1.873 1.873 0 0 1 2.693-1.115l.291.16c.764.415 1.6-.42 1.184-1.185l-.159-.291a1.873 1.873 0 0 1 1.116-2.693l.318-.094c.835-.246.835-1.428 0-1.674l-.319-.094a1.873 1.873 0 0 1-1.115-2.692l.16-.292c.415-.764-.42-1.6-1.185-1.184l-.291.159A1.873 1.873 0 0 1 8.93 1.945l-.094-.319zm-2.633-.283c.527-1.79 3.065-1.79 3.592 0l.094.319a.873.873 0 0 0 1.255.52l.292-.16c1.64-.892 3.434.901 2.54 2.541l-.159.292a.873.873 0 0 0 .52 1.255l.319.094c1.79.527 1.79 3.065 0 3.592l-.319.094a.873.873 0 0 0-.52 1.255l.16.292c.893 1.64-.902 3.434-2.541 2.54l-.292-.159a.873.873 0 0 0-1.255.52l-.094.319c-.527 1.79-3.065 1.79-3.592 0l-.094-.319a.873.873 0 0 0-1.255-.52l-.292.16c-1.64.893-3.433-.902-2.54-2.541l.159-.292a.873.873 0 0 0-.52-1.255l-.319-.094c-1.79-.527-1.79-3.065 0-3.592l.319-.094a.873.873 0 0 0 .52-1.255l-.16-.292c-.892-1.64.902-3.433 2.541-2.54l.292.159a.873.873 0 0 0 1.255-.52l.094-.319z"/>
+                                <path fill-rule="evenodd" d="M8 5.754a2.246 2.246 0 1 0 0 4.492 2.246 2.246 0 0 0 0-4.492zM4.754 8a3.246 3.246 0 1 1 6.492 0 3.246 3.246 0 0 1-6.492 0z"/>
+                                </svg>
+                        </div>
+                    </div>
+                  
+            </nav>
+
+
+            <div class="filter-container">
+                    <div class="mobile-tab-wrap">
+                    <div class="filter-tabs justify-start lg:justify-center mt-2 font-bold text-center text-sm">
+                       <a v-for="menu in $store.state.kategori" :key="toId(menu)" href="#"
+                       
+                        :class="($store.state.menu_tab == toId(menu)) ? 'border-b-2 text-primary border-primary animate-pulse' : ''" 
+                
+                            v-scroll-to="{
+                                    el: `#${toId(menu)}`,
+                                    offset: -120,
+                                    duration: 600,
+                                }" 
+                            
+                            class="uppercase p-3 lg:px-5">{{menu}}</a>  
+                    </div>
+                    
+                    </div>
+            </div>
+   </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        toId(string){
+            return string.replace(' ','').replace('&','').replace('-','').replace('/','').replace(' ','')
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+
+.filter-container .mobile-tab-wrap {
+  overflow: auto;
+}
+
+.filter-container .mobile-tab-wrap .filter-tabs {
+  width: auto;
+  min-width: 1000px;
+  background: transparent;
+}
+
+.filter-tabs {
+  display: flex;
+  flex-wrap: wrap;
+  background: #f2f2f2;
+}
+
+</style>
---
+++ Nuxt/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="w-full  min-h-screen bg-theme_primary flex flex-wrap justify-center items-center pt-32">
+    
+    <div v-for="menu in $store.state.kategori" :key="toId(menu)" 
+    ref="element"  
+    :id="toId(menu)" 
+    class="h-screen w-full p-3 lg:px-20">
+      <h1 class="font-bold text-xl uppercase">
+        {{ menu }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  scrollToTop: true,
+  layout: 'app',
+  mounted() {
+      var that = this;
+        window.addEventListener("scroll", function() {
+          for(let index = 0; index < that.$store.state.kategori.length;index++){
+            let top = that.$refs.element[index].getBoundingClientRect().top;
+              if(top <= 120){
+                  that.$store.commit('setMenuTab',that.toId(that.$store.state.kategori[index]))
+              }
+          }
+        
+      });
+  }
+  ,methods:{
+    toId(string){
+         return  string.replace(' ','').replace('&','').replace('-','').replace('/','').replace(' ','')
+      }
+  }
+}
+</script>
+
+    
